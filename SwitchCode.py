687f75c0eb922eca14bb71382fed150d97a6d884
Zawwar Kamran <51718813+zawwarkamran@users.noreply.github.com>
2021-05-25 19:35:05 -0400

    Update SwitchCode.py

--- SwitchCode.py
+++ SwitchCode.py
@@ -1,12 +1,11 @@
 import requests
 import RPi.GPIO as GPIO
 
-ledPin = 12 # define ledPin
 sensorPin = 11 # define sensorPin
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(ledPin, GPIO.OUT)
+	#GPIO.setup(ledPin, GPIO.OUT)
 	GPIO.setup(sensorPin, GPIO.IN) # set sensorPin to INPUT mode
 
 def loop():	
@@ -23,9 +22,9 @@ def destroy():
 # use PHYSICAL GPIO Numbering # set ledPin to OUTPUT mode
 
 if __name__ == '__main__': # Program entrance 
-print('Program is starting...')
-setup()
-try:
-	loop()
-except KeyboardInterrupt: # Press ctrl-c to end the program.
-	destroy()
+	print('Program is starting...')
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt: # Press ctrl-c to end the program.
+		destroy()
