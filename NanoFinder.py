2c06c7b4c241b5762117cb0e5940971d8bbbe1b7
Zawwar Kamran <51718813+zawwarkamran@users.noreply.github.com>
2021-05-28 15:53:41 -0400

    Update NanoFinder.py

--- NanoFinder.py
+++ NanoFinder.py
@@ -4,20 +4,15 @@ import requests
 
 nl = Nanoleaf("192.168.2.218")
 
-def switch():
-	while True:
-		response = requests.get("http://192.168.2.229")
-		print(response.content['motion'])
-		state = response.content['motion']
-		if state == 1:
-			nl.power_off()
-			print("turning off")
-			time.sleep(5)
-		else:
-			nl.power_on()
 
-if __name__ == '__main__':
-	try:
-		switch()
-	except:
-		print("error")
+while True:
+	response = requests.get("http://192.168.2.229")
+	print(response.content['motion'])
+	state = response.content['motion']
+	if state == 1:
+		nl.power_off()
+		print("turning off")
+		time.sleep(5)
+	else:
+		nl.power_on()
+
