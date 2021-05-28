ed881adf706e74a46f7122712bb4d1124d5947e2
Zawwar Kamran <51718813+zawwarkamran@users.noreply.github.com>
2021-05-27 20:01:17 -0400

    added esp8266 interface and request to esp8266

--- NanoFinder.py
+++ NanoFinder.py
@@ -1,7 +1,9 @@
 from nanoleafapi import discovery, Nanoleaf 
 import time
+import requests
 
-
+response = requests.get("http://192.168.2.229")
+print(response)
 nl = Nanoleaf("192.168.2.218")
 
 nl.power_off()
--- SwitchCodeESP8266/SwitchCodeESP8266.ino
+++ SwitchCodeESP8266/SwitchCodeESP8266.ino
@@ -1,19 +1,35 @@
+#include <ESP8266WiFi.h>
+
 int PIRsensor = 13;
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(PIRsensor, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
-}
+  WiFi.begin("edbtz", "sajwarcho202");
 
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+//void loop() {
+//  Serial.println(WiFi.localIP());
+//}
 void loop() {
   long state = digitalRead(PIRsensor);
   if (state == HIGH){
     Serial.println("motion");
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
     }
     else {
       Serial.println("no motion");
-      digitalWrite(LED_BUILTIN, LOW);
+      digitalWrite(LED_BUILTIN, HIGH);
     }
 }
