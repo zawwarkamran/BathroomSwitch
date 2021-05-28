d7cb2ca1aad8d6468eedfa407dd0fcbc870dc779
Zawwar Kamran <51718813+zawwarkamran@users.noreply.github.com>
2021-05-28 15:57:40 -0400

    Update NanoFinder.py

--- NanoFinder.py
+++ NanoFinder.py
@@ -1,13 +1,15 @@
 from nanoleafapi import discovery, Nanoleaf 
 import time
 import requests
+import json
 
 nl = Nanoleaf("192.168.2.218")
 
 
 while True:
 	response = requests.get("http://192.168.2.229")
-	print(type(response.text))
+	motion_data = json.loads(response.text)
+	motion_data['variables']['motion']
 	state = response.content['motion']
 	if state == 1:
 		nl.power_off()
