5c26345d818656afb432d6ad9dbd155678b45ab7
Zawwar Kamran <51718813+zawwarkamran@users.noreply.github.com>
2021-06-11 00:24:03 -0400

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 found_devices = asyncio.run(Discover.discover())
 print(found_devices)
 
-@app.route('/')
-def home():
-	status = requests.get("http://192.168.2.229")
-	status = json.loads(status.text)
-	state = status['variables']['motion']
-	if motion == 1:
-		asyncio.run(switch_on())
-    return "Hello, World"
+
+# @app.route('/')
+# def home():
+# 	status = requests.get("http://192.168.2.229")
+# 	status = json.loads(status.text)
+# 	state = status['variables']['motion']
+# 	if motion == 1:
+# 		asyncio.run(switch_on())
 
 if __name__ == '__main__':
 	app.run(debug=True, host="0.0.0.0")
