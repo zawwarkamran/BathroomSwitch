4146a86ce9c3bd6d72fd5f1fd3f3f3bd6a677e6a
Zawwar Kamran <51718813+zawwarkamran@users.noreply.github.com>
2021-06-12 16:25:56 -0400

    Update automationfuncs.py

--- automationfuncs.py
+++ automationfuncs.py
@@ -7,9 +7,9 @@ import asyncio
 async def switch_on(switch_ip):
 	dimmer = kasa.SmartDimmer(switch_ip)
 	await asyncio.coroutine(dimmer.update)()
-	await asynicio.coroutine(dimmer.turn_on)()
+	await asyncio.coroutine(dimmer.turn_on)()
 
 async def switch_off(switch_ip):
 	dimmer = kasa.SmartDimmer(switch_ip)
 	await asyncio.coroutine(dimmer.update)()
-	await asynicio.coroutine(dimmer.turn_off)()
+	await asyncio.coroutine(dimmer.turn_off)()
